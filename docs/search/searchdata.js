cf41d00a24ac8b79cb2a9907b7c0a305893405b2
user <user@gray.lan>
2025-06-25 22:59:05 -0500

    Use README.md as doxygen main page

--- docs/search/searchdata.js
+++ docs/search/searchdata.js
@@ -2,20 +2,23 @@ var indexSectionsWithContent =
 {
   0: "b",
   1: "b",
-  2: "b"
+  2: "b",
+  3: "b"
 };
 
 var indexSectionNames =
 {
   0: "all",
   1: "files",
-  2: "functions"
+  2: "functions",
+  3: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
   1: "Files",
-  2: "Functions"
+  2: "Functions",
+  3: "Pages"
 };
 
